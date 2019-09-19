1e4ad2a18f8c527cbd852fe2aaaf7467a6055b13
bohdan_taraba <bohdantaraba@gmail.com>
2019-09-19 12:30:23 +0200

    andoid input working

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -16,12 +16,33 @@ public class PlayerMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float turn = Input.GetAxis("Horizontal");
+        float turn = 0;
+#if UNITY_EDITOR
+        turn = Input.GetAxis("Horizontal");
+#endif
+
+#if UNITY_ANDROID
+        foreach (Touch touch in Input.touches)
+        {
+            
+            if ((touch.position.x > Screen.width / 2) && (touch.phase == TouchPhase.Stationary))
+            {
+                turn = 1;
+            }else if ((touch.position.x < Screen.width / 2) && (touch.phase == TouchPhase.Stationary))
+            {
+                turn = -1;
+            }
+        }
+#endif
         rigid.AddTorque(-torque * turn);
     }
+
+
 
     void unfreezeRotation()
     {
         rigid.constraints = RigidbodyConstraints2D.None;
     }
+
+
 }
